80114282dc5a70c33c0214044ec12cf106eb9d9e
nickolay.osiniy <nickolay.osiniy@gmail.om>
2018-07-25 12:24:57 +0300

    tested

--- from_sybase_iq_to_csv.py
+++ from_sybase_iq_to_csv.py
@@ -1,11 +1,11 @@
 import os
-import pyodbc
 import zipfile
 import csv
+import pymssql
 import sys
 from contextlib import closing
 
-driver = '{SQL Server}'
+
 server = '192.168.1.1'
 user = 'user'
 password = 'password'
@@ -18,8 +18,7 @@ sql_query = ''' select * from airflow_table'''
 
 class IqToCsv:
     """партиционно записывает данные с сервера в текстовый файл"""
-    def __init__(self, driver, srv, usr, passw, delimiter, filepath):
-        self.driver = driver
+    def __init__(self, srv, usr, passw, delimiter, filepath):
         self.srv = srv
         self.usr = usr
         self.passw = passw
@@ -28,26 +27,27 @@ class IqToCsv:
 
     def from_iq_to_csv(self,):
         try:
-            with closing(pyodbc.connect(pyodbc.connect(self.driver, self.srv, self.usr, self.passw))) as conn:
+            with closing(pymssql.connect(self.srv, self.usr, self.passw, tds_version='4.2',
+                                         conn_properties='', charset='cp866')) as conn:
                 cursor = conn.cursor()
                 cursor.execute(sql_query)
                 cnt = 0
-                with open(self.filepath, "w") as f:
+                with open(self.filepath, "w", newline='') as f:
                     csv_writer = csv.writer(f, delimiter='|')
                     csv_writer.writerow([field[0] for field in cursor.description])
-                    for i, chunk in enumerate(chunks(cursor)):
+                    for i, chunk in enumerate(self.chunks(cursor)):
                         cnt += len(chunk)
                         f.write('\n'.join([self.delimiter.join([str(r) for r in row]) for row in chunk]))
                         f.write('\n')
-                        log.info('{0} pack unloaded. [{1}] total rows'.format(i, cnt))
+                        print('{qnt} pack unloaded. {count} total rows'.format(qnt=i, count=cnt))
                     f.flush()
                     cursor.close()
                     conn.commit()
                     conn.close()
-                    log.info("Dumping finished")
-                    arch = zipfile.ZipFile(os.path.splitext(self.filepath)[0] + '.zip', "w", zipfile.ZIP_DEFLATED)
-                    arch.write(self.filepath, os.path.basename(self.filepath))
-                    os.remove(self.filepath)
+                    print("Dumping finished")
+                arch = zipfile.ZipFile(os.path.splitext(self.filepath)[0] + '.zip', "w", zipfile.ZIP_DEFLATED)
+                arch.write(self.filepath, os.path.basename(self.filepath))
+                os.remove(self.filepath)
 
         except Exception:
             print(" Error while writing from database to csv")
@@ -62,7 +62,7 @@ class IqToCsv:
             yield rows
 
 
-writer = IqToCsv(driver=driver, srv=server, usr=user, passw=password, delimiter=delimiter, filepath=local_filepath)
+writer = IqToCsv(srv=server, usr=user, passw=password, delimiter=delimiter, filepath=local_filepath)
 
 
 if __name__ == "__main__":
